e6347b6a08f2ef7a6d309090a3db2fb6318a5ab1
GreenT <treorey@gmail.com>
2018-06-17 12:29:55 +0700

    finish functional test

--- functional_tests.py
+++ functional_tests.py
@@ -32,18 +32,30 @@ class NewVisitorTest(unittest.TestCase):
    
     self.assertEqual(input_answer.get_attribute('placeholder'),'Enter your answer')
     input_answer.send_keys('True')
-
-
-    time.sleep(5)
+    time.sleep(3)
 
     submit_button = self.browser.find_element_by_name('create_button')
     submit_button.click()
-    time.sleep(5)
+    time.sleep(2)
 
-    find_text = self.browser.find_elements_by_xpath("//*[contains(text(), 'Dog have 4 legs')]").text
+    find_text = self.browser.find_element_by_id('q2')
 
-    self.assertEqual(find_text, 'Dog have 4 legs')
+    self.assertEqual(find_text.get_attribute('name'), 'Dog have 4 legs')
+    find_text.click()
+    time.sleep(2)
 
+    find_ans = self.browser.find_element_by_id('choice')
+    find_ans.send_keys('True')
+    submit_button2 = self.browser.find_element_by_id('create_ans')
+    submit_button2.click()
+ 
+    check_url_results= self.browser.current_url
+    self.assertRegex(check_url_results, '/keepresults/*')
+    time.sleep(2)
+ 
+
+
+    
     self.fail('Finish the test!')
 
 if __name__ == '__main__':
